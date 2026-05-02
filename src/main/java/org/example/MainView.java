80b5aa29faba96667833771c6391ba8344e482bd
Shr-eyaK <shreyakannan492@gmail.com>
2026-05-01 22:22:48 -0400

    Add header bar with title and layout structure

--- src/main/java/org/example/MainView.java
+++ src/main/java/org/example/MainView.java
@@ -2,14 +2,20 @@ package org.example;
 
 import javafx.geometry.Insets;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.Priority;
+
 
 public class MainView {
 
     public BorderPane createUI() {
         BorderPane root = new BorderPane();
 
+        //Sidebar
         VBox box = new VBox();
         box.setSpacing(10);
         box.setPadding(new Insets(20));
@@ -22,8 +28,24 @@ public class MainView {
         Label personal = new Label("Personal");
 
         box.getChildren().addAll(title,all,work,school,personal);
-
         root.setLeft(box);
+
+        //Header
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        hbox.setPadding(new Insets(20));
+
+        Label title2 = new Label("Task Manager");
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        Button addTaskButton = new Button("+ Add Task");
+
+        hbox.getChildren().addAll(title2,addTaskButton,spacer);
+        root.setTop(hbox);
+
         return root;
+
     }
 }
